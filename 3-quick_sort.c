83d550aa183fddfefd38f5198118d2f4a6abc9c9
safa riahi <safa@DESKTOP-RFB3EIJ.localdomain>
2024-04-04 15:00:49 +0200

    quick

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,67 +1,78 @@
 #include "sort.h"
 
 /**
- * lomuto_part - function topartition an arrayinto two parts.
- * @array: the array topartition it.
- * @left: left case of array.
- * @right: right case of array
- * @size: size of array
- * Return: return an array part
-*/
-int lomuto_part(int *array, int left, int right, size_t size)
+  * quick_sort - ...
+  * @array: ...
+  * @size: ...
+  *
+  * Return: Nothing!
+  */
+void quick_sort(int *array, size_t size)
 {
-int i, j, pivot, tmp;
+	if (!array || size < 2)
+		return;
 
-pivot = array[right];
-i = left;
-for (j = left; j <= right; j++)
-{
-if (array[j] < pivot)
-{ 
-tmp = array[i];
-array[i] = array[j];
-array[j] = tmp;
-print_array(array, size);
-i++;
-}
-else
-continue;
-}
-tmp = array[right];
-array[right] = array[i];
-array[i] = tmp;
-print_array(array, size);
-return (i);
-}
-
- /**
-  * quick_sort_rec - function that sorts an array of integers in ascendig order recursevly.
-  * @array: array of integers.
-  * @left: the lower index of array;
-  * @right: the higher index of array;
-  *  @size: size of array.
- */
-
-void quick_sort_rec(int *array, int left, int right, size_t size)
-{
-int index;
-
-if( left < right)
-{
-index = lomuto_part(array, left, right, size);
-quick_sort_rec(array, left,  index - 1, size);
-quick_sort_rec(array, index + 1, right, size);
-}
+	quick_sort_rec(array, 0, size - 1, size);
 }
 
 /**
- * void quick_sort - funtion sorts an array i ascending order.
- * @array: array to sort it.
- * @size: the size of array. 
-*/
-void quick_sort(int *array, size_t size)
+  * quick_sort_rec - ...
+  * @array: ...
+  * @lower: ...
+  * @higher: ...
+  * @size: ...
+  *
+  * Return: Nothing!
+  */
+void quick_sort_rec(int *array, int lower, int higher, size_t size)
 {
-if (!array || size < 2)
-return;
-quick_sort_rec(array, 0, size - 1, size);
+	int l_p = 0;
+
+	if (lower < higher)
+	{
+		l_p = lomuto_partition(array, lower, higher, size);
+		quick_sort_rec(array, lower, l_p - 1, size);
+		quick_sort_rec(array, l_p + 1, higher, size);
+	}
+}
+
+/**
+  * lomuto_partition - ...
+  * @array: ...
+  * @lower: ...
+  * @higher: ...
+  * @size: ...
+  *
+  * Return: Nothing!
+  */
+int lomuto_partition(int *array, int lower, int higher, size_t size)
+{
+	int i = 0, j = 0, pivot = 0, aux = 0;
+
+	pivot = array[higher];
+	i = lower;
+
+	for (j = lower; j < higher; ++j)
+	{
+		if (array[j] < pivot)
+		{
+			aux = array[i];
+			array[i] = array[j];
+			array[j] = aux;
+
+			if (aux != array[i])
+				print_array(array, size);
+
+			++i;
+		}
+	}
+
+	aux = array[i];
+	array[i] = array[higher];
+	array[higher] = aux;
+
+	if (aux != array[i])
+		print_array(array, size);
+
+	return (i);
 }
